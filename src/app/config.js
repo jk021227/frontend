d7c78bf53b0fe2dee0367c0ab85f65a73520a55d
jk021227 <yk2606@nyu.edu>
2024-12-07 13:12:02 -0500

    gotten rid of dash

--- src/app/config.js
+++ src/app/config.js
@@ -3,7 +3,7 @@
 //     frontendUrl: "https://legally-chemie.vercel.app/",
 // };
 export default {
-    apiUrl: "https://sea-turtle-app-akcpx.ondigitalocean.app/",   
-    frontendUrl: "https://lionfish-app-hldzt.ondigitalocean.app/",
+    apiUrl: "https://sea-turtle-app-akcpx.ondigitalocean.app",   
+    frontendUrl: "https://lionfish-app-hldzt.ondigitalocean.app",
 };
 // NOTE: BACKEND URLS ARE HARDCODED IN THE backend/server.py file
