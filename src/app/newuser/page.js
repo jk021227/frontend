f9e7cb5b0dcdc0bdaae41a55bbeeefacdbab9754
jk021227 <yk2606@nyu.edu>
2024-12-07 14:31:25 -0500

    getting rid of trailing slash

--- src/app/newuser/page.js
+++ src/app/newuser/page.js
@@ -29,7 +29,7 @@ export default function NewUser() {
       try {
         console.log("this is the api url", apiUrl);
         console.log("this is the selected skin type", selectedSkinType);
-        const response = await fetch(apiUrl + `/${selectedSkinType}/`, {
+        const response = await fetch(apiUrl + `/${selectedSkinType}`, {
           method: "POST",
           credentials: 'include',
           headers: {
