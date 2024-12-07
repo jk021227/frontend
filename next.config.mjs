b2801f273bc714937cd74a0bfce78d1c51acf188
jk021227 <yk2606@nyu.edu>
2024-12-07 12:18:12 -0500

    configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'standalone',
+  };
+  
+  export default nextConfig;  
