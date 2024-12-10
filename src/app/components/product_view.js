b1fd1ad75d76d0699693e818c84fe93733b7b624
jk021227 <yk2606@nyu.edu>
2024-12-10 17:50:48 -0500

    product)view has community rating now

--- src/app/components/product_view.js
+++ src/app/components/product_view.js
@@ -25,12 +25,12 @@ const apiUrl = config.apiUrl;
 const frontendUrl = config.frontendUrl;
 
 const labels = {
-    0: 'No Rating',
-    1: 'Horrible',
-    2: 'Poor',
-    3: 'Ok',
-    4: 'Good',
-    5: 'Excellent',
+    0: 'no rating',
+    1: 'horrible',
+    2: 'poor',
+    3: 'ok',
+    4: 'good',
+    5: 'excellent',
 };
 
 function getLabelText(value) {
@@ -54,44 +54,44 @@ function getLabelText(value) {
 
 const ProductCard = ({ selectedProduct, onDelete, onClose, isThemeChanged, day }) => {
     const [rating, setRating] = useState(0);
+    const [oldRating, setOldRating] = useState(0);
+    const [communityRating, setCommunityRating] = useState({});
     const [hover, setHover] = useState(-1);
     const [error, setError] = useState(null);
+    const [isUpdating, setIsUpdating] = useState(false);
+
+    const fetchRating = async () => {
+        try {
+            console.log("This is the selected product id: ", selectedProduct.id);
+            console.log("Fetching rating for the product");
+            const response = await fetch(
+                apiUrl + `/${day}/products/${selectedProduct.id}/rating`,
+                { method: "GET", credentials: "include" }
+            );
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch rating");
+            }
+
+            const data = await response.json();
+            console.log("This is the rating for the product fetched from the function: ", data);
+            setRating(data.user_rating);
+            setOldRating(data.user_rating);
+            setCommunityRating(data.community_rating);
+        } catch (err) {
+            setError(err.message);
+        }
+    };
 
     useEffect(() => {
-        const fetchRating = async () => {
-            try {
-                console.log("This is the selected product id: ", selectedProduct.id);
-                console.log("fetching rating for the product");
-                const response = await fetch(
-                    apiUrl + `/${day}/products/${selectedProduct.id}/rating`,
-                    {   method: "GET",
-                        credentials: "include" } 
-                );
-
-                if (!response.ok) {
-                    throw new Error("Failed to fetch rating");
-                    
-                }
-
-                const data = await response.json();
-                console.log("This is the rating for the product fetched from the function: ", data.rating);
-                setRating(data.rating); 
-            } catch (err) {
-                setError(err.message); 
-            }
-        };
-
-        fetchRating();
+        if (selectedProduct) {
+            fetchRating();
+        }
     }, [selectedProduct, day]);
 
     if (!selectedProduct) {
-        return null
+        return null;
     }
-
-    /**
-     * @function handleDelete
-     * @brief Confirms and deletes the selected product from the user's routine.
-     */
 
     const handleDelete = () => {
         const confirmed = window.confirm("Are you sure you want to delete this product?");
@@ -100,66 +100,91 @@ const ProductCard = ({ selectedProduct, onDelete, onClose, isThemeChanged, day }
         }
     };
 
-    const handleRatingChange = (newValue) => {
-        setRating(newValue);
-        console.log('Updated rating:', newValue);
-        console.log('type of the newValue', typeof newValue);
+    const handleRatingChange = async (newValue) => {
+        setIsUpdating(true);
+        try {
+            const response = await fetch(
+                apiUrl + `/${day}/products/${selectedProduct.id}/${newValue}/${oldRating}`,
+                {
+                    method: 'PATCH',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ rating: newValue }),
+                }
+            );
     
-        fetch(apiUrl + `/${day}/products/${selectedProduct.id}/${newValue}`, {
-          method: 'PATCH',
-          credentials: 'include',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ rating: newValue }),
-          credentials: 'include',
-        })
-          .then((response) => {
-            console.log('Response status:', response.status);  
-            return response.json();
-          })
-          .then((data) => {
-            console.log('Data received from server:', data); 
-            console.log('Data type:', typeof data); 
-          })
-          .catch((err) => {
-            console.error('Error updating rating:', err);
-          });
-      };
+            if (!response.ok) {
+                throw new Error('Failed to update rating');
+            }
+    
+            const data = await response.json();
+            console.log('Rating updated successfully:', data);
+    
+            setOldRating(newValue);
+    
+            await fetchRating();
+        } catch (error) {
+            console.error('Error updating rating:', error);
+        }
+        finally {
+            setIsUpdating(false);
+        }
+    };
+    
     
 
     console.log("Selected Product:", selectedProduct);
 
     return (
-        <div className="product-view"
-        style={{ backgroundColor: isThemeChanged ? '#00CEF7' : '#FFAADF'}}>
-            <button onClick={onClose} className="close-button"
-            style={{ backgroundColor: isThemeChanged ?  '#03045E' : '#ff0090',
-            cursor: isThemeChanged 
-            ? `url('/select2.png') 2 2, pointer` 
-            : `url('/select1.png') 2 2, pointer`,}}>X</button>
+        <div
+            className="product-view"
+            style={{
+                backgroundColor: isThemeChanged ? "#00CEF7" : "#FFAADF",
+            }}
+        >
+            <button
+                onClick={onClose}
+                className="close-button"
+                style={{
+                    backgroundColor: isThemeChanged ? "#03045E" : "#ff0090",
+                    cursor: isThemeChanged
+                        ? `url('/select2.png') 2 2, pointer`
+                        : `url('/select1.png') 2 2, pointer`,
+                }}
+            >
+                X
+            </button>
             <div className="product-card">
-                <h3 className="product-name"
-                style={{ color: isThemeChanged ? '#00028E' : '#9c0060'}}>{selectedProduct.name}</h3>
+                <h3
+                    className="product-name"
+                    style={{ color: isThemeChanged ? "#00028E" : "#9c0060" }}
+                >
+                    {selectedProduct.name}
+                </h3>
                 <div className="image-container">
-                    <img src={selectedProduct.image} alt={selectedProduct.name}/>
+                    <img src={selectedProduct.image} alt={selectedProduct.name} />
                 </div>
-                <p className="product-brand"><strong>Brand:</strong> {selectedProduct.brand}</p>
-                <p className="product-description"><strong>Description:</strong> {selectedProduct.description}</p>
+                <p className="product-brand">
+                    <strong>Brand:</strong> {selectedProduct.brand}
+                </p>
+                <p className="product-description">
+                    <strong>Description:</strong> {selectedProduct.description}
+                </p>
                 <details className="product-ingredients">
-                    <summary><strong>Ingredients:</strong></summary>
-                    {selectedProduct.ingredients.join(', ')}
+                    <summary>
+                        <strong>Ingredients:</strong>
+                    </summary>
+                    {selectedProduct.ingredients.join(", ")}
                 </details>
-                <div className="product-rating">
-                    <Typography component="legend" sx={{ fontFamily: 'Montserrat, sans-serif' }}>
+                <div className="horizontal-line"></div>
+                <div className="product-rating" style={{ textAlign: "left", paddingLeft: "5px" }}>
+                    <Typography component="legend" sx={{ fontFamily: "Montserrat, sans-serif", fontSize: "15px" }}>
                         <strong>My Rating:</strong>
                     </Typography>
-                    <Box sx={{ '& > legend': { mt: 1 } }}>
-                        {rating !== null && (
-                            <Box>
-                                {labels[hover !== -1 ? hover : rating]}
-                            </Box>
-                        )}
+                    <Box sx={{ "& > legend": { mt: 1 } }}>
+                        {rating !== null && <Box>{labels[hover !== -1 ? hover : rating]}</Box>}
                         <Rating
                             name="product-rating"
                             value={rating}
@@ -169,37 +194,72 @@ const ProductCard = ({ selectedProduct, onDelete, onClose, isThemeChanged, day }
                             onChangeActive={(event, newHover) => {
                                 setHover(newHover);
                             }}
+                            disabled={isUpdating}
                             emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
                             icon={
-                                <StarIcon 
-                                    fontSize="inherit" 
-                                    style={{ color: isThemeChanged ? '#03045E' : '#FF3EB5' }} 
+                                <StarIcon
+                                    fontSize="inherit"
+                                    style={{
+                                        color: isThemeChanged ? "#03045E" : "#FF3EB5",
+                                    }}
                                 />
                             }
                         />
                     </Box>
                 </div>
-
-                <button 
-                onClick={handleDelete} 
-                style={{
-                    position: 'relative',
-                    marginLeft: '320px',
-                    marinTop: '50px',
-                    backgroundImage: isThemeChanged
-                    ? 'url(/trashcan2.png)'
-                    : 'url(/trashcan1.png)', 
-                    backgroundSize: 'contain', 
-                    backgroundRepeat: 'no-repeat', 
-                    backgroundPosition: 'center', 
-                    width: '32px',
-                    height: '32px',
-                    border: 'none',                            
-                    cursor: isThemeChanged 
-                    ? `url('/select2.png') 2 2, pointer` 
-                    : `url('/select1.png') 2 2, pointer`,}}>
-                </button>
-
+                <div className="community-rating" style={{ textAlign: "left", paddingLeft: "5px" }}>
+                    <Typography component="legend" sx={{ fontFamily: "Montserrat, sans-serif", fontSize: "15px" }}>
+                        <strong>Community Ratings:</strong>
+                    </Typography>
+                    {Object.keys(communityRating).map((skinType) => (
+                        <div
+                            key={skinType}
+                            className="community-rating-item"
+                            style={{
+                                display: "flex",
+                                alignItems: "center", 
+                                marginTop: "5px", 
+                            }}
+                        >
+                            <span style={{ marginRight: "10px" }}>♡ {skinType}:</span>
+                            <Rating
+                                name={`community-rating-${skinType}`}
+                                value={communityRating[skinType]}
+                                precision={0.1}
+                                readOnly
+                                emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                                icon={
+                                    <StarIcon
+                                        fontSize="inherit"
+                                        style={{
+                                            color: isThemeChanged ? "#03045E" : "#FF3EB5",
+                                        }}
+                                    />
+                                }
+                            />
+                        </div>
+                    ))}
+                </div>
+                <button
+                    onClick={handleDelete}
+                    style={{
+                        position: "relative",
+                        marginLeft: "320px",
+                        marginTop: "0",
+                        backgroundImage: isThemeChanged
+                            ? "url(/trashcan2.png)"
+                            : "url(/trashcan1.png)",
+                        backgroundSize: "contain",
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center",
+                        width: "32px",
+                        height: "32px",
+                        border: "none",
+                        cursor: isThemeChanged
+                            ? `url('/select2.png') 2 2, pointer`
+                            : `url('/select1.png') 2 2, pointer`,
+                    }}
+                ></button>
             </div>
         </div>
     );
